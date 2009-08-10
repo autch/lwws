3341132d2f19144afcef98f8b6c8d6b29fac7b84
autch <autch@1207b28a-9a7b-4cb1-bc77-8e6633a4c0a7>
2009-08-10 09:47:17 +0000

    * 予報詳細画面のモックを追加

---
+++ src/net/autch/android/lwws/ForecastDetail.java
@@ -0,0 +1,7 @@
+package net.autch.android.lwws;
+
+import android.app.Activity;
+
+public class ForecastDetail extends Activity {
+
+}
---
+++ src/net/autch/android/lwws/ForecastList.java
@@ -0,0 +1,13 @@
+package net.autch.android.lwws;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ForecastList extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+	
+}
