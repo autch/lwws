3889c81b8467402172b1c1f1ecd146297ab73da2
autch <autch@1207b28a-9a7b-4cb1-bc77-8e6633a4c0a7>
2009-08-20 10:00:22 +0000

    * パーサ取っ掛かり

--- src/net/autch/webservice/lwws/ForecastParser.java
+++ src/net/autch/webservice/lwws/ForecastParser.java
@@ -1,8 +1,11 @@
 package net.autch.webservice.lwws;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.util.Xml;
 
@@ -15,9 +18,26 @@ public class ForecastParser {
 	private Forecast forecast;
 	private XmlPullParser parser;
 	
-	public Forecast parse(InputStream in) {
+	public Forecast parse(InputStream in) throws XmlPullParserException, IOException {
 		parser = Xml.newPullParser();
 		forecast = new Forecast();
+
+		parser.setInput(new InputStreamReader(in));
+		int eventType = parser.getEventType();
+
+		while (eventType != XmlPullParser.END_DOCUMENT) {
+			String tagName = parser.getName();
+			switch(eventType) {
+			case XmlPullParser.START_TAG:
+				if(tagName.equals("area")) {
+				}
+				break;
+			case XmlPullParser.END_TAG:
+				if(tagName.equals("source")) {
+				}
+			}
+			eventType = parser.next();
+		}
 
 		return forecast;
 	}
